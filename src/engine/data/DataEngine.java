70e43ccc0cd777d45b850ff73d651a0217306587
Aunmag <aunmag@gmail.com>
2017-07-27 05:52:01 +0500

    Set engine title (#8)

--- src/engine/data/DataEngine.java
+++ src/engine/data/DataEngine.java
@@ -2,7 +2,7 @@ package engine.data;
 
 public class DataEngine {
 
-    public static final String name = "A Java Engine 2D";
+    public static final String name = "Nightingale Engine";
     public static final String version = "0.4.0";
     public static final String license = "Apache 2.0";
     public static final String developer = "Aunmag";
@@ -10,7 +10,7 @@ public class DataEngine {
     public static final String title = String.format("%s v%s", name, version);
     public static final String titleFull = String.format("%s by %s", title, developer);
     public static final String description = (
-            "This is my engine which is powered by LWJGL."
+            "This is my 2D engine which is powered by LWJGL."
     );
 
 }
