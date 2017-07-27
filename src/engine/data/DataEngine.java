033aeed52e68a521f9acf8a618f2fc2f5a1fd14a
Aunmag <aunmag@gmail.com>
2017-07-27 05:48:19 +0500

    Set engine title

--- src/engine/data/DataEngine.java
+++ src/engine/data/DataEngine.java
@@ -2,7 +2,7 @@ package engine.data;
 
 public class DataEngine {
 
-    public static final String name = "A Java Engine 2D";
+    public static final String name = "Nightingale Engine";
     public static final String version = "0.4.0";
     public static final String license = "Apache 2.0";
     public static final String developer = "Aunmag";
