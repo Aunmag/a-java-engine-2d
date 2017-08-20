ca8d759f51b7afd68fd85d448d5b7087291ad663
Aunmag <aunmag@gmail.com>
2017-08-20 11:36:52 +0500

    Fixed world timer (#30)

--- src/nightingale/basics/BaseWorld.java
+++ src/nightingale/basics/BaseWorld.java
@@ -1,11 +1,13 @@
 package nightingale.basics;
 
+import nightingale.data.DataTime;
+
 public abstract class BaseWorld implements BaseOperative {
 
     private long timeCurrent = 0;
 
     public void update() {
-        timeCurrent++;
+        timeCurrent += DataTime.getTimePassed();
     }
 
     public abstract void render();
