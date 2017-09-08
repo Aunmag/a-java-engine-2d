2a61a95d6d7d06b5529784533f3313d21075eacc
Aunmag <aunmag@gmail.com>
2017-09-09 02:10:50 +0500

    Set version

--- src/nightingale/data/DataEngine.java
+++ src/nightingale/data/DataEngine.java
@@ -3,7 +3,7 @@ package nightingale.data;
 public class DataEngine {
 
     public static final String name = "Nightingale Engine";
-    public static final String version = "0.24.0";
+    public static final String version = "0.25.0";
     public static final String license = "Apache 2.0";
     public static final String developer = "Aunmag";
     public static final String developerEmail = "aunmag@gmail.com";
