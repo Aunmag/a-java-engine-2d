7b6f5879a0f05d1a84d8e083575f7ceceed22be5
Aunmag <aunmag@gmail.com>
2017-09-16 03:51:41 -0700

    Added smooth float (inertia) (#48)

---
+++ src/nightingale/utilities/FloatSmooth.java
@@ -0,0 +1,74 @@
+package nightingale.utilities;
+
+public class FloatSmooth {
+
+    private float valueInitial = 0;
+    private float valueCurrent = 0;
+    private float valueTarget = 0;
+    private float flexDegree = 1;
+    private long timeInitial = System.currentTimeMillis();
+    private long timeDuration;
+
+    public FloatSmooth(long timeDuration) {
+        this.timeDuration = timeDuration;
+    }
+
+    public void update(long timeCurrent) {
+        if (isTargetReached()) {
+            return;
+        }
+
+        long timePassed = timeCurrent - timeInitial;
+        float timeDone = (float) timePassed / (float) timeDuration;
+
+        if (timeDone >= 1) {
+            reachTargetNow();
+        } else {
+            timeDone = (float) Math.pow(timeDone, flexDegree);
+            float valueRange = valueTarget - valueInitial;
+            float valueIncrease = valueRange * timeDone;
+            valueCurrent = valueInitial + valueIncrease;
+        }
+    }
+
+    public void reachTargetNow() {
+        valueCurrent = valueTarget;
+    }
+
+    /* Setters */
+
+    public void setValueTarget(float valueTarget, long timeInitial) {
+        if (valueTarget != this.valueTarget) {
+            valueInitial = this.valueCurrent;
+            this.valueTarget = valueTarget;
+            setTimeInitial(timeInitial);
+        }
+    }
+
+    public void setFlexDegree(float flexDegree) {
+        this.flexDegree = flexDegree;
+    }
+
+    public void setTimeDuration(long timeDuration) {
+        this.timeDuration = timeDuration;
+    }
+
+    public void setTimeInitial(long timeInitial) {
+        this.timeInitial = timeInitial;
+    }
+
+    /* Getters */
+
+    public float getValueCurrent() {
+        return valueCurrent;
+    }
+
+    public boolean isTargetReached() {
+        return valueCurrent == valueTarget;
+    }
+
+    public long getTimeDuration() {
+        return timeDuration;
+    }
+
+}
