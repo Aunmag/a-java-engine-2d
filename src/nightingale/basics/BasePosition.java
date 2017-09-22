2982b249b77e96b0fa7a0aa13709152e07ec653a
Aunmag <aunmag@gmail.com>
2017-09-22 14:46:53 -0700

    Stored sin and cos for base position radians (#58)

--- src/nightingale/basics/BasePosition.java
+++ src/nightingale/basics/BasePosition.java
@@ -4,11 +4,13 @@ import nightingale.utilities.UtilsMath;
 
 public class BasePosition extends BasePoint {
 
-    private float radians = 0;
+    private float radians;
+    private float cos;
+    private float sin;
 
     public BasePosition(float x, float y, float radians) {
         super(x, y);
-        this.radians = radians;
+        setRadians(radians);
     }
 
     public final void addRadiansCarefully(float radians) {
@@ -18,13 +20,27 @@ public class BasePosition extends BasePoint {
     /* Setters */
 
     public void setRadians(float radians) {
+        if (radians == this.radians) {
+            return;
+        }
+
         this.radians = radians;
+        cos = (float) Math.cos(radians);
+        sin = (float) Math.sin(radians);
     }
 
     /* Getters */
 
     public float getRadians() {
         return radians;
+    }
+
+    public float getCos() {
+        return cos;
+    }
+
+    public float getSin() {
+        return sin;
     }
 
 }
