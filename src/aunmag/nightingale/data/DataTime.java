68c68a0978eae27b5527a5ec932859c1833adfb0
Aunmag <aunmag@gmail.com>
2017-09-26 19:04:34 -0700

    Added FPS limit config (#66)

--- src/aunmag/nightingale/data/DataTime.java
+++ src/aunmag/nightingale/data/DataTime.java
@@ -2,8 +2,6 @@ package aunmag.nightingale.data;
 
 public class DataTime {
 
-    private static final int fpsLimit = 60;
-    private static final float timeFrameDuration = 1000f / fpsLimit;
     private static float timeDelta = 0;
     private static long timeCurrent = System.currentTimeMillis();
     private static long timePassed = 0;
@@ -20,14 +18,6 @@ public class DataTime {
     }
 
     /* Getters */
-
-    public static int getFpsLimit() {
-        return fpsLimit;
-    }
-
-    public static float getTimeFrameDuration() {
-        return timeFrameDuration;
-    }
 
     public static float getTimeDelta() {
         return timeDelta;
