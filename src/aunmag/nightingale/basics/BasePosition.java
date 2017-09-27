d0d36d9538c8092d05d724d92f47b55be41c89bf
Aunmag <aunmag@gmail.com>
2017-09-26 18:41:53 -0700

    Removed sine and cosine variables (#65)

--- src/aunmag/nightingale/basics/BasePosition.java
+++ src/aunmag/nightingale/basics/BasePosition.java
@@ -5,12 +5,10 @@ import aunmag.nightingale.utilities.UtilsMath;
 public class BasePosition extends BasePoint {
 
     private float radians;
-    private float cos;
-    private float sin;
 
     public BasePosition(float x, float y, float radians) {
         super(x, y);
-        setRadians(radians);
+        this.radians = radians;
     }
 
     public final void addRadiansCarefully(float radians) {
@@ -20,27 +18,13 @@ public class BasePosition extends BasePoint {
     /* Setters */
 
     public void setRadians(float radians) {
-        if (radians == this.radians) {
-            return;
-        }
-
         this.radians = radians;
-        cos = (float) Math.cos(radians);
-        sin = (float) Math.sin(radians);
     }
 
     /* Getters */
 
     public float getRadians() {
         return radians;
-    }
-
-    public float getCos() {
-        return cos;
-    }
-
-    public float getSin() {
-        return sin;
     }
 
 }
