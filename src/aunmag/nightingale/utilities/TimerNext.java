3ace904ffc35ced24026901f144ec5f009a5f173
Aunmag <aunmag@gmail.com>
2017-10-23 00:04:42 +0500

    Added timer utilities

---
+++ src/aunmag/nightingale/utilities/TimerNext.java
@@ -0,0 +1,36 @@
+package aunmag.nightingale.utilities;
+
+public class TimerNext {
+
+    private int timeDuration = 0;
+    private long timeNext = 0;
+    private boolean isNow = false;
+
+    public TimerNext(int timeDuration) {
+        setTimeDuration(timeDuration);
+    }
+
+    public void update(long timeCurrent) {
+        isNow = false;
+
+        if (timeCurrent > timeNext) {
+            timeNext = timeCurrent + timeDuration;
+            isNow = true;
+        }
+    }
+
+    /* Setters */
+
+    public void setTimeDuration(int timeDuration) {
+        timeNext -= this.timeDuration;
+        this.timeDuration = timeDuration;
+        timeNext += this.timeDuration;
+    }
+
+    /* Getters */
+
+    public boolean isNow() {
+        return isNow;
+    }
+
+}
