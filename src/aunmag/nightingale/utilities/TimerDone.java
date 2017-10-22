34a5f17642c0f5c6ad1599c36db55c236d4d842b
Aunmag <aunmag@gmail.com>
2017-10-22 12:06:23 -0700

    Added timer utilities (#71)

---
+++ src/aunmag/nightingale/utilities/TimerDone.java
@@ -0,0 +1,33 @@
+package aunmag.nightingale.utilities;
+
+public class TimerDone {
+
+    private int timeDuration = 0;
+    private long timeInitial = 0;
+    private long timeTarget = 0;
+
+    public TimerDone(int timeDuration) {
+        setTimeDuration(timeDuration);
+    }
+
+    private void updateTimeTarget() {
+        timeTarget = timeInitial + timeDuration;
+    }
+
+    public boolean calculateIsDone(long timeCurrent) {
+        return timeTarget <= timeCurrent;
+    }
+
+    /* Setters */
+
+    public void setTimeDuration(int timeDuration) {
+        this.timeDuration = timeDuration;
+        updateTimeTarget();
+    }
+
+    public void setTimeInitial(long timeInitial) {
+        this.timeInitial = timeInitial;
+        updateTimeTarget();
+    }
+
+}
