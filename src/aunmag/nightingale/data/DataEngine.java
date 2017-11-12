60a62d178ea47ed6115afcc45568ddea931f8160
Aunmag <aunmag@gmail.com>
2017-11-12 01:57:09 -0800

    Added time flow util (#88)

--- src/aunmag/nightingale/data/DataEngine.java
+++ src/aunmag/nightingale/data/DataEngine.java
@@ -3,7 +3,7 @@ package aunmag.nightingale.data;
 public class DataEngine {
 
     public static final String name = "Nightingale Engine";
-    public static final String version = "0.38.6";
+    public static final String version = "0.38.7";
     public static final String license = "Apache 2.0";
     public static final String developer = "Aunmag";
     public static final String developerEmail = "aunmag@gmail.com";
