b3a8b986d75ccfcd7ce6ef3c41e506415a3ca34b
Aunmag <aunmag@gmail.com>
2017-12-24 11:54:07 +0500

    Cleaned code

--- src/main/java/aunmag/nightingale/Application.java
+++ src/main/java/aunmag/nightingale/Application.java
@@ -56,11 +56,6 @@ public abstract class Application {
 
             if (frameRate.calculateIsNow(timeCurrent)) {
                 engineUpdate();
-
-                if (Input.getMouseVelocity().x() != 0) {
-                    System.out.println(time.getDelta());
-                }
-
                 engineRender();
             }
         }
