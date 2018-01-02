60f01b1a27998af206284607cf4b14c28469cd35
Aunmag <aunmag@gmail.com>
2018-01-02 19:02:36 +0500

    Changed implementation of GUI page

--- src/main/java/aunmag/nightingale/gui/GuiPage.java
+++ src/main/java/aunmag/nightingale/gui/GuiPage.java
@@ -1,24 +1,25 @@
 package aunmag.nightingale.gui;
 
-import aunmag.nightingale.Application;
-import aunmag.nightingale.structures.Texture;
 import aunmag.nightingale.utilities.UtilsGraphics;
 import org.lwjgl.opengl.GL11;
 
+import java.util.ArrayList;
+
 public class GuiPage {
 
-    private GuiLabel[] labels;
-    private GuiButton[] buttons;
-    private Texture wallpaper;
-
-    public GuiPage(GuiLabel[] labels, GuiButton[] buttons, Texture wallpaper) {
-        this.labels = labels;
-        this.buttons = buttons;
-        this.wallpaper = wallpaper;
-    }
+    private ArrayList<GuiLabel> labels = new ArrayList<>();
+    private ArrayList<GuiButton> buttons = new ArrayList<>();
 
     public void open() {
         GuiManager.open(this);
+    }
+
+    public void add(GuiLabel label) {
+        labels.add(label);
+    }
+
+    public void add(GuiButton button) {
+        buttons.add(button);
     }
 
     public void update() {
@@ -28,10 +29,6 @@ public class GuiPage {
     }
 
     public void render() {
-        if (wallpaper != null) {
-            renderWallpaper();
-        }
-
         fillScreen();
 
         for (GuiLabel label: labels) {
@@ -45,18 +42,8 @@ public class GuiPage {
 
     private void fillScreen() {
         GL11.glColor4f(0.2f, 0.2f, 0.2f, 0.6f);
-
-        int width = (int) Application.getWindow().getWidth();
-        int height = (int) Application.getWindow().getHeight();
-
         UtilsGraphics.drawPrepare();
-        UtilsGraphics.drawQuad(0, 0, width, height, true, false);
-    }
-
-    private void renderWallpaper() {
-        wallpaper.bind();
-        Application.getShader().setUniformProjection(Application.getWindow().projection);
-        wallpaper.render();
+        UtilsGraphics.fillScreen();
     }
 
 }
