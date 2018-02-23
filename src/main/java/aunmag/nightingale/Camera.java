3f69f76cfc47e5550b4c67f0ff2aac97ef11ad7a
Aunmag <aunmag@gmail.com>
2018-02-23 13:53:25 +0500

    Added mount util for camera (#113)

--- src/main/java/aunmag/nightingale/Camera.java
+++ src/main/java/aunmag/nightingale/Camera.java
@@ -2,11 +2,11 @@ package aunmag.nightingale;
 
 import aunmag.nightingale.audio.AudioMaster;
 import aunmag.nightingale.basics.BasePosition;
+import aunmag.nightingale.utilities.Mount;
 import aunmag.nightingale.utilities.UtilsMath;
 import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 import org.joml.Vector2f;
-import org.joml.Vector2fc;
 import org.joml.Vector3f;
 
 public class Camera extends BasePosition {
@@ -19,25 +19,21 @@ public class Camera extends BasePosition {
     private float scaleWindow;
     private float scaleZoom = ZOOM_MIN * 2;
     private float scaleFull;
-    private float radiansOffset = 0;
-    private Vector2f offset = new Vector2f(0, 0);
+    public final Mount mount;
 
     Camera() {
         super(0, 0, 0);
         setDistanceView(40);
+        mount = new Mount(this, null);
     }
 
     public void update() {
-        float x = getX() + offset.x();
-        float y = getY() + offset.y();
-        float radians = UtilsMath.correctRadians(
-                getRadians() + radiansOffset - UtilsMath.PIx0_5
-        );
+        float radians = UtilsMath.correctRadians(getRadians() - UtilsMath.PIx0_5);
 
         viewMatrix = new Matrix4f(Application.getWindow().projection);
         viewMatrix.rotateZ(-radians);
         viewMatrix.scale(scaleFull);
-        viewMatrix.translate(-x, -y, 0);
+        viewMatrix.translate(-getX(), -getY(), 0);
 
         Vector3f orientation = new Vector3f(0, 1, 0);
         Quaternionf quaternion = new Quaternionf(0, 0, 0);
@@ -64,23 +60,7 @@ public class Camera extends BasePosition {
         return projection;
     }
 
-    void resetOffsets() {
-        offset.set(0, 0);
-        radiansOffset = 0;
-    }
-
     /* Setters */
-
-    public void addOffset(float radians, float distance, boolean useZoom) {
-        if (useZoom) {
-            distance /= scaleFull;
-        }
-
-        radians = UtilsMath.correctRadians(radians + getRadians() + radiansOffset);
-        float x = distance * (float) Math.cos(radians);
-        float y = distance * (float) Math.sin(radians);
-        offset.add(x, y);
-    }
 
     public void setDistanceView(float distanceView) {
         this.distanceView = distanceView;
@@ -91,10 +71,6 @@ public class Camera extends BasePosition {
     public void setScaleZoom(float scaleZoom) {
         this.scaleZoom = UtilsMath.limitNumber(scaleZoom, ZOOM_MIN, ZOOM_MAX);
         updateScaleFull();
-    }
-
-    public void addRadiansOffset(float radiansOffset) {
-        this.radiansOffset += radiansOffset;
     }
 
     /* Getters */
@@ -113,14 +89,6 @@ public class Camera extends BasePosition {
 
     public float getScaleFull() {
         return scaleFull;
-    }
-
-    public Vector2fc getOffset() {
-        return offset.toImmutable();
-    }
-
-    public float getRadiansOffset() {
-        return radiansOffset;
     }
 
 }
